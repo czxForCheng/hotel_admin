24ef09f2a75cbc2fa6aaaa21fc8f6840c9dfcb45
yangmin123123 <yangmin123123@outlook.com>
2024-02-13 15:16:10 +0800

    1.1.31 -- 禁用账号理由，是否禁止提现

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,7 +2,7 @@ const config = {
     terminal: 1, //终端
     title: '后台管理系统', //网站默认标题
     version: '1.1.31', //版本号
-    baseUrl: '',
+    baseUrl: '/',
     assetsUrl: '',
     urlPrefix: 'api', //请求默认前缀x
     timeout: 20 * 1000 //请求超时时长
