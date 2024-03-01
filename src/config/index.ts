bd77d3838246e254e772f95e92ff3eea54009179
yangmin123123 <yangmin123123@outlook.com>
2024-03-01 11:29:55 +0800

    1.1.34 -- 添加注册验证码开关、工作台页面跳转

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,8 +2,7 @@ const config = {
     terminal: 1, //终端
     title: '后台管理系统', //网站默认标题
     version: '1.1.34', //版本号
-    // baseUrl: 'http://8.210.20.168:8089',
-    baseUrl: 'http://172.16.0.108:8082/',
+    baseUrl: '/',
     assetsUrl: '',
     urlPrefix: 'api', //请求默认前缀x
     timeout: 20 * 1000 //请求超时时长
